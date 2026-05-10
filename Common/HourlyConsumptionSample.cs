0de97bda99d66d1036dc0af9a55fc21ea7540af4
Aleksandar <aleksandarjevtic04@gmail.com>
2026-05-10 16:31:37 +0200

    Zadatak 2

---
+++ Common/HourlyConsumptionSample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class HourlyConsumptionSample
+    {
+        [DataMember]
+        public DateTime TimestampUtc { get; set; }
+
+        [DataMember]
+        public DateTime TimestampLocal { get; set; }
+
+        [DataMember]
+        public int Hour { get; set; }
+
+        [DataMember]
+        public double ActualMW { get; set; }
+
+        [DataMember]
+        public double ForecastMW { get; set; }
+
+        [DataMember]
+        public string CountryCode { get; set; }
+
+        [DataMember]
+        public int RowIndex { get; set; }
+    }
+}
---
+++ Common/SessionMeta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class SessionMeta
+    {
+        [DataMember]
+        public string CountryCode { get; set; }
+
+        [DataMember]
+        public DateTime Date { get; set; }
+
+        [DataMember]
+        public string SourceFileName { get; set; }
+
+        [DataMember]
+        public int TotalSamples { get; set; }
+    }
+}
